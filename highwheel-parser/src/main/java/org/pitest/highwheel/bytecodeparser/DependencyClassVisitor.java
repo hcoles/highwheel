17fb08a62ce28c1e6a1f73b49d3ea54be10ae060
hcoles <henry.coles@googlemail.com>
2015-12-23 20:38:18 +0000

    upgrade to asm 5.0.4 api

--- highwheel-parser/src/main/java/org/pitest/highwheel/bytecodeparser/DependencyClassVisitor.java
+++ highwheel-parser/src/main/java/org/pitest/highwheel/bytecodeparser/DependencyClassVisitor.java
@@ -25,7 +25,7 @@ class DependencyClassVisitor extends ClassVisitor {
 
   public DependencyClassVisitor(final ClassVisitor visitor,
       final AccessVisitor typeReceiver, NameTransformer nameTransformer) {
-    super(Opcodes.ASM4, visitor);
+    super(Opcodes.ASM5, visitor);
     this.dependencyVisitor = filterOutJavaLangObject(typeReceiver);
     this.nameTransformer = nameTransformer;
   }
